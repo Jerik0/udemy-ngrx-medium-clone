462033c9480d931ecbf7897ba487259c3e9475e8
Jonathan C <jcarrico1985@live.com>
2022-11-20 19:57:29 -0600

    udemy ngrx course - created initial interfaces for currentUser and registerRequest. Through section 2 part 12.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,7 +14,12 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule,
     AppRoutingModule,
     AuthModule,
-    StoreModule.forRoot([])
+    StoreModule.forRoot([]),
+    // Instrumentation must be imported after importing StoreModule (config is optional)
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // retains last 25 states
+      logOnly: environment.production // restrict extension to log-only mode
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
